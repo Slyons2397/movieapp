cdfc5075b5fc51cc1a071ad2755ba626f2e402ef
SamLyons <SamLyons@Samuels-MacBook-Air.local>
2023-02-10 20:41:01 -0500

    added more to sections

--- social-media-app/src/components/MovieCard.js
+++ social-media-app/src/components/MovieCard.js
@@ -3,12 +3,17 @@ import React from "react";
 const MovieCard = ({ movieDesc, image }) => {
   return (
     <>
-      <div class="card" style={{ width: "18rem" }}>
-        <img src={image} class="card-img-top" alt="..." />
+      <div className="card bg-black mb-5 pb-3" style={{ width: "18rem" }}>
+        <img
+          src={image}
+          className="card-img-top mt-3"
+          style={{ height: "400px" }}
+          alt="..."
+        />
         <div class="card-body text-center">
-          <p class="card-text">{movieDesc}</p>
-          <div className="btn btn-success">Click Here For More</div>
+          <p class="card-text text-white">{movieDesc}</p>
         </div>
+        <div className="btn btn-outline-info">Watch Now</div>
       </div>
     </>
   );
--- social-media-app/src/pages/HomePage.js
+++ social-media-app/src/pages/HomePage.js
@@ -1,32 +1,31 @@
-import React, { useState } from "react";
+import React from "react";
 import Nav from "../components/Nav";
 import MovieCard from "../components/MovieCard";
 
 const HomePage = () => {
   return (
-    <>
+    <div className="bg-dark">
       <Nav />
-      <div className="row gap-1 justify-content-center mt-5">
-        <h1 className="ms-5 text-danger">Horror:</h1>
-        <MovieCard image="../../images/applepage.png" movieDesc="k" />
+      <div className="row gap-4 justify-content-center mt-5">
+        <h1 className="ms-5 text-danger display-2 fw-semibold">Horror</h1>
         <MovieCard
-          image="../../images/applepage.png"
-          movieDesc="A great new flick featuring top actors"
+          image="../../images/themenu.png"
+          movieDesc="A young couple travels to a remote island to eat at an exclusive restaurant where the chef has prepared a lavish menu, with some shocking surprises."
         />
         <MovieCard
-          image="../../images/applepage.png"
-          movieDesc="A great new flick featuring top actors"
+          image="../../images/megan.png"
+          movieDesc="A robotics engineer at a toy company builds a life-like doll that begins to take on a life of its own."
         />
         <MovieCard
-          image="../../images/applepage.png"
-          movieDesc="A great new flick featuring top actors"
+          image="../../images/smile.jpeg"
+          movieDesc="After witnessing a bizarre, traumatic incident involving a patient, a psychiatrist becomes increasingly convinced she is being threatened by an uncanny entity."
         />
         <MovieCard
-          image="../../images/applepage.png"
-          movieDesc="A great new flick featuring top actors"
+          image="../../images/nope.jpeg"
+          movieDesc="The residents of a lonely gulch in inland California bear witness to an uncanny and chilling discovery."
         />
       </div>
-    </>
+    </div>
   );
 };
 
